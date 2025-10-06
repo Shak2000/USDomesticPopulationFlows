f23678c24e2fae93d2a00c037f0c33360ddd98a5
Shak Ragoler <shak.ragoler@gmail.com>
2025-10-05 23:23:47 -0700

    Adding AGI per capita

--- import-data.sql
+++ import-data.sql
@@ -1,2 +1,4 @@
 .mode csv
 .import --skip 1 stateoutflow2122.csv StateOutflow2122
+ALTER TABLE StateOutflow2122
+    ADD COLUMN agi_per_capita INTEGER GENERATED ALWAYS AS (CASE WHEN num_people = 0 THEN 0 ELSE agi / num_people END);
